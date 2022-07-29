1af0c162f6c2bfa43328f12162a85cbd6419377a
Edgar Gago Carrillo <edgargc.upc@gmail.com>
2022-07-29 21:34:35 +0200

    feat: create a new fetch for the detail view

--- src/StarWars/StarWars.js
+++ src/StarWars/StarWars.js
@@ -5,9 +5,9 @@ export const StarWars = () => {
 
   // useEffect --> this is going to udpate the componet (the first time) or (whenever we tell it to do so)
   useEffect(() => {
-    fetch("https://www.swapi.tech/api/people/4")
+    fetch("https://www.swapi.tech/api/people?page=1&limit=10")
       .then((response) => response.json())
-      .then((result) => setState(result.result))
+      .then((result) => setState(result.results))
       .catch((error) => console.log("error", error));
   }, []);
 
@@ -15,7 +15,32 @@ export const StarWars = () => {
 
   return (
     <div>
-      <h1>{`It was born in: ${state.properties.birth_year}`}</h1>
+      {state.map((person) => (
+        <div key={person.name}>
+          <ComponentCard person={person} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const ComponentCard = ({ person }) => {
+  const [personData, setPersonData] = useState("");
+
+  useEffect(() => {
+    fetch(`https://www.swapi.tech/api/people/${person.uid}`)
+      .then((response) => response.json())
+      .then((result) => setPersonData(result.result))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  if (!personData) return null;
+
+  return (
+    <div>
+      <h1>{personData?.properties?.name}</h1>
+      <p>{personData?.properties?.birth_year}</p>
+      <p>{personData?.properties?.height}</p>
     </div>
   );
 };
